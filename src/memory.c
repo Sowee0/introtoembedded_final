320d20f8e76faf6cbac36d16d40e7fecdc5abed4
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2020-09-05 14:00:02 -0600

    Adding the implementation for my_memset and my_memzero

--- src/memory.c
+++ src/memory.c
@@ -50,6 +50,7 @@ void clear_all(char * ptr, unsigned int size){
 
 uint8_t * my_memmove(uint8_t * src, uint8_t * dst, size_t length){
 
+//missing a verification as not to overlap
   for( size_t i = 0 ; i < length; i++){
     *(src + i) = *(dst + i);
     *dst = *ptr;
@@ -72,9 +73,23 @@ uint8_t * my_memcopy(uint8_t * src, uint8_t * dst, size_t length){
 
 uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value){
 
+  uint8_t * position;
+  for(size_t i = 0; i < length; i++){
+    position = src + i;
+    *position = value;
+    
+  }
+
 }
 
 uint8_t * my_memzero(uint8_t * src, size_t length){
+
+  uint8_t * position;
+  for(size_t i = 0; i < length; i++){
+    position = src + i;
+    *position = 0;
+    
+  }
 
 }
 
