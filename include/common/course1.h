4b2c7c97e1d4c38ffe47cab7bc2c1d3543fc2aae
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2020-09-07 15:29:31 -0600

    Adding some libraries

--- include/common/course1.h
+++ include/common/course1.h
@@ -20,6 +20,9 @@
 #define __COURSE1_H__
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 #define DATA_SET_SIZE_W (10)
 #define MEM_SET_SIZE_B  (32)
@@ -30,6 +33,9 @@
 #define TEST_ERROR          (1)
 #define TEST_NO_ERROR       (0)
 #define TESTCOUNT           (8)
+
+#define BASE_16 (16)
+#define BASE_10 (10)
 
 /**
  * @brief function to run course1 materials
