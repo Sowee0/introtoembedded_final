50a05494362ef202177eb23502dcf6a1d20d41e4
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2020-09-07 18:11:06 -0600

    Getting rid of the old main()

--- src/stats.c
+++ src/stats.c
@@ -27,32 +27,9 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+//void main() {}
 
-  int size = SIZE;
-
-  /* Other Variable Declarations Go Here */
-
-
-
-  /* Statistics and Printing Functions Go Here */
-
-  //program flow
-  print_array(test, size);
-  sort_array(test, size);
-  print_array(test, size);
-
-  print_statistics(test, size);
-
-
-
-}
 
 void print_statistics(unsigned char array[], unsigned int size){
  
